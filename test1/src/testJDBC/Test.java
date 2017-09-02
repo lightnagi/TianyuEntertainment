20c1158219928c8cd0a74983373d25204ffcf1d1
nagi <tzh1202@126.com>
2017-09-02 09:03:59 +0800

    project test

---
+++ test1/src/testJDBC/Test.java
@@ -0,0 +1,28 @@
+package testJDBC;
+
+import java.sql.*;
+
+public class Test {
+	public static void main(String[] args){
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection( //改为对应的账号和密码
+					"jdbc:mysql://localhost:3306/tianyu?characterEncoding=utf8", "root", "admin");
+			Statement st = conn.createStatement();
+			ResultSet rs = st.executeQuery("select * from 用户表");
+			while(rs.next()){
+				System.out.println(rs.getString("账号") + "\t" + rs.getString("余额"));
+			}
+			rs.close();
+			st.close();
+			conn.close();
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
